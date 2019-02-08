a9bd805c5076f5e17104a28ddc3a8f5bedb75e12
JonHGee <jonhgee@gmail.com>
2019-02-07 18:59:22 -0800

    Add files via upload

--- python/gui.py
+++ python/gui.py
@@ -1,23 +1,21 @@
 import tkinter as tk
 import tkinter.font as tkFont
 import time
-from servosix import ServoSix
 import iPill
 
 ###############################################################################
 # Parameters and global variables
 
-ss = ServoSix()
-period = 2
-
 # Default font size
-font_size = -24
+font_size = 10
 
 # Declare global variables
 root = None
 dfont = None
 frame = None
 dtime = None
+last = time.localtime()
+flag = False
 
 # Global variable to remember if we are fullscreen or windowed
 fullscreen = False
@@ -67,10 +65,11 @@ def update():
 
     global root
     global dtime
-
+    global last
+    global flag
+    
     # Get local time
     local_time = time.localtime()
-
     # Convert time to 12 hour clock
     hours = local_time.tm_hour
     if hours > 12:
@@ -79,16 +78,38 @@ def update():
     # Add leading 0s
     shours = str(hours)
     smin = str(local_time.tm_min)
+    ssec = str(local_time.tm_sec)
     if hours < 10:
         shours = '0' + shours
     if local_time.tm_min < 10:
         smin = '0' + smin
+    if local_time.tm_sec < 10:
+        ssec = '0' + ssec
 
     # Construct string out of time
-    dtime.set(shours + ':' + smin)
-
+    dtime.set(shours + ':' + smin + ':' + ssec)
+    if abs(last.tm_sec - local_time.tm_sec) > 5:
+        flag = True    
+    
+    if local_time.tm_sec%10 < 5 and flag == True:
+        button_dispense.config(state="normal")
+        button_dispense.grid(row=1, column=0, padx=5, pady=5)
+    else:
+        button_dispense.grid_forget()
+        
+    
     # Schedule the poll() function for another 500 ms from now
     root.after(500, update)
+
+def dispense():
+    global last
+    global flag
+    button_dispense.grid_forget()
+    button_dispense.config(state="disabled")
+    flag = False
+    last = time.localtime()
+    iPill.dispensePill()
+    
 
 ###############################################################################
 # Main script
@@ -102,7 +123,7 @@ frame = tk.Frame(root, bg='blue')
 
 # Lay out the main container (expand to fit window)
 frame.pack(fill=tk.BOTH, expand=1)
-
+frame.config(cursor='none')
 # Variables for holding temperature and light data
 dtime = tk.StringVar()
 
@@ -113,9 +134,9 @@ button_dfont = tkFont.Font(size=font_size)
 # Create widgets
 label_time = tk.Label(  frame, 
                         textvariable=dtime, 
-                        font=time_dfont, 
-                        fg='red', 
-                        bg='black')
+                        font=("Courier", 60), 
+                        fg='white', 
+                        bg='blue')
 button_quit = tk.Button(frame, 
                         text="Quit", 
                         font=button_dfont, 
@@ -127,21 +148,24 @@ button_quit = tk.Button(frame,
                         
 button_dispense = tk.Button(frame, 
                         text="Dispense Pill", 
-                        font=button_dfont, 
-                        command=iPill.dispensePill,
-                        borderwidth=0,
-                        highlightthickness=0, 
+                        font=("Courier", 14), 
+                        command=dispense,
+                        #command=iPill.dispensePill,
+                        borderwidth=2,
+                        highlightthickness=0,
+                        height=5,
+                        width=20, 
                         fg='gray10',
                         bg='white')
 
 # Lay out widgets in a grid in the frame
 label_time.grid(row=0, column=0, padx=0, pady=20)
 button_quit.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
-button_dispense.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+button_dispense.grid(row=1, column=0, padx=5, pady=5)
 
 # Make it so that the grid cells expand out to fill window
 frame.rowconfigure(0, weight=10)
-frame.rowconfigure(1, weight=1)
+frame.rowconfigure(1, weight=10)
 frame.columnconfigure(0, weight=1)
 
 # Bind F11 to toggle fullscreen and ESC to end fullscreen
