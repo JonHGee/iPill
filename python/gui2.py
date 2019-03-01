1bdf69c9b0fe457bb3c72b0abc5389db4afd591b
Jonathan Gee <jonhgee@gmail.com>
2019-03-01 12:46:02 -0800

    Add files via upload

--- python/gui2.py
+++ python/gui2.py
@@ -1,8 +1,6 @@
-# Multi-frame tkinter application v2.3
 import tkinter as tk
 import tkinter.font as tkFont
 import time
-from servosix import ServoSix
 import iPill
 
 # Declare global variables
@@ -17,19 +15,22 @@ class SampleApp(tk.Tk):
     def __init__(self):
         global time_dfont
         global button_dfont
+        global label_dfont
         global dtime
+        global pill
+        global dispensed
         root = tk.Tk.__init__(self)
         time_dfont = tkFont.Font(family='Courier New', size=font_size, weight=tkFont.BOLD)
         button_dfont = tkFont.Font(size=font_size)
+        label_dfont = tkFont.Font(size=font_size)
         dtime = tk.StringVar()
-        #self.update
-        self.geometry = ('1700x1700')
+        pill = iPill.pillInfo()
+        dispensed = False
         self._frame = None
         self.switch_frame(StartPage)
         self.bind('<F11>', self.toggle_fullscreen)
         self.bind('<Escape>', self.end_fullscreen)
         self.bind('<Configure>', self.resize)
-        self.toggle_fullscreen
         self.after(20,self.update)
 
     def toggle_fullscreen(self, event=None):
@@ -58,8 +59,11 @@ class SampleApp(tk.Tk):
 
         global time_dfont
         global button_dfont
+        global label_dfont
         new_size = -max(12, int((self._frame.winfo_height() / 6)))
         time_dfont.configure(size=new_size)
+        new_size = -max(12, int((self._frame.winfo_height() / 12)))
+        label_dfont.configure(size=new_size)
         new_size = -max(12, int((self._frame.winfo_height() / 8)))
         button_dfont.configure(size=new_size)
         
@@ -67,7 +71,7 @@ class SampleApp(tk.Tk):
     # Read values from the sensors at regular intervals
     def update(self):
         global dtime
-        
+        global dispensed
         # Get local time
         local_time = time.localtime()
         # Convert time to 12 hour clock
@@ -87,8 +91,9 @@ class SampleApp(tk.Tk):
             ssec = '0' + ssec
 
         # Construct string out of time
-        dtime.set(shours + ':' + smin + ':' + ssec)   
-        self.after(500, self.update)
+        if dispensed == False:
+            dtime.set(shours + ':' + smin + ':' + ssec)   
+        self.after(700, self.update)
     
     def switch_frame(self, frame_class):
         """Destroys current frame and replaces it with a new one."""
@@ -114,29 +119,59 @@ class StartPage(tk.Frame):
 class pharmframe(tk.Frame):
     global time_dfont
     global button_dfont
+    global label_dfont
+    global pill
     def __init__(self, master):
         pharm = tk.Frame.__init__(self, master)
-        tk.Label(self, font=time_dfont,text="Pharmacist Mode",highlightthickness=0).grid(row=0, columnspan = 2,sticky=tk.E)
+        
+        tk.Label(self, font=time_dfont,text="Pharmacist Mode",highlightthickness=0).grid(row=0, columnspan = 3,sticky=tk.E+tk.W)
+        
+        self.pname = tk.Entry(self, font =label_dfont, text="John Doe")
+        self.pname.grid(row=1, column = 1, columnspan = 2, pady=15,sticky=tk.W+tk.E)
+        tk.Label(self, text="Patient Name", font =label_dfont).grid(row=1, column = 0,sticky=tk.W+tk.E)
+        self.pinfo = tk.Entry(self, font =label_dfont, text="Ibuprofen")
+        self.pinfo.grid(row=2, column = 1, columnspan = 2,pady=15, sticky=tk.W+tk.E)
+        tk.Label(self, text="Prescription Info", font =label_dfont).grid(row=2, column = 0, sticky=tk.W+tk.E)
+        self.dinfo = tk.Entry(self, font =label_dfont, text="Two tablets three times a day")
+        self.dinfo.grid(row=3, column = 1, columnspan = 2,pady=15, sticky=tk.W+tk.E)
+        tk.Label(self, text="Dosage Info", font =label_dfont).grid(row=3, column = 0, sticky=tk.W+tk.E)
+        self.numpills = tk.Spinbox(self, from_=1, to = 99, width=5,font = label_dfont)
+        self.numpills.grid(row=4, column = 1, pady=15, sticky=tk.W)
+        tk.Label(self, text="Number of Pills", font =label_dfont).grid(row=4, column = 0, sticky=tk.W+tk.E)
+        tk.Button(self, text="Encode Info", font=button_dfont, command=lambda:self.setinfo()).grid(row=4,column=2)
+        
         tk.Button(self, text="Return to start page", font=button_dfont,highlightthickness=0,
-                  command=lambda: master.switch_frame(StartPage)).grid(row=4, columnspan = 2, sticky=tk.W+tk.E)
-        pname = tk.Entry(self, font =('Courier New', 16)).grid(row=1, column = 1, sticky=tk.W+tk.E)
-        tk.Label(self, text="Patient Name", font =('Courier New', 16)).grid(row=1, column = 0, sticky=tk.W+tk.E)
-        pname = tk.Entry(self, font =('Courier New', 16)).grid(row=2, column = 1, sticky=tk.W+tk.E)
-        tk.Label(self, text="Prescription Info", font =('Courier New', 16)).grid(row=2, column = 0, sticky=tk.W+tk.E)
-        pname = tk.Entry(self, font =('Courier New', 16)).grid(row=3, column = 1, sticky=tk.W+tk.E)
-        tk.Label(self, text="Dosage Info", font =('Courier New', 16)).grid(row=3, column = 0, sticky=tk.W+tk.E)
-
-
+                  command=lambda: master.switch_frame(StartPage)).grid(row=5, columnspan = 3, sticky=tk.W+tk.E)
+        
+        self.pinfo.delete(0, tk.END)
+        self.pname.delete(0, tk.END)
+        self.dinfo.delete(0, tk.END)
+        
+        self.pname.insert(0,"John Doe")
+        self.pinfo.insert(0,"Ibuprofen 400mg")
+        self.dinfo.insert(0,"1 pill taken 3x per day")
+        
+        self.columnconfigure(1, weight=0)
+        self.columnconfigure(0, weight=10)
+        self.columnconfigure(2, weight=10)
+        
+    def setinfo(self):
+        pill.setinfo(self.pname.get(),self.pinfo.get(),self.dinfo.get(),self.numpills.get())
+        
 class userframe(tk.Frame):
     def __init__(self, master):
         global time_dfont
         global button_dfont
         global dtime
+        global pill
         homeuser = tk.Frame.__init__(self, master)
         dtime = tk.StringVar()
-        
         tk.Label(self, text="Home User Mode", font=time_dfont,highlightthickness=0).grid(row=0, columnspan=2,column=0, padx=0, pady=0)
-        
+        tk.Label(self, text = "Patient Name: "+pill.pname, font=label_dfont).grid(row=2,column=0)
+        tk.Label(self, text = "Pill info: "+pill.pinfo, font=label_dfont).grid(row=3,column=0,columnspan=2)
+        tk.Label(self, text = "Dosage: "+pill.dinfo, font=label_dfont).grid(row=4,column=0,columnspan=2)
+        self.pill_label = tk.Label(self, text = "Pills left: "+pill.getpills(), font=label_dfont)
+        self.pill_label.grid(row=2,column=1)
         button_quit = tk.Button(self, 
                         text="Quit", 
                         font=button_dfont, 
@@ -149,28 +184,38 @@ class userframe(tk.Frame):
         button_dispense = tk.Button(self, 
                         text="Dispense Pill", 
                         font=button_dfont, 
-                        command=iPill.dispensePill,
+                        command=self.dispense,
                         borderwidth=0,
                         highlightthickness=0, 
                         fg='gray10',
                         bg='white')
-        label_time = tk.Label(self, 
+        self.label_time = tk.Label(self, 
                         textvariable=dtime, 
                         font=time_dfont,
                         highlightthickness=0,
                         fg='black')
         
-        label_time.grid(row=1, column=0,columnspan=2, padx=0, pady=20)
-        button_quit.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W+tk.E)
-        button_dispense.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
+        
+        
+        self.label_time.grid(row=1, column=0,columnspan=2, padx=0, pady=0)
+        button_quit.grid(row=5, column=1, padx=5, pady=5, sticky=tk.W+tk.E)
+        button_dispense.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
         self.rowconfigure(0, weight=10)
         self.rowconfigure(1, weight=10)
         self.columnconfigure(1, weight=10)
         self.columnconfigure(0, weight=10)
-        master.after(20, self.update)
+
+    def dispense(self):
+        global dispensed
+        global dtime
+        dispensed = True
+        dtime.set("Dispensing Pill")
+        pill.dispense()
+        iPill.dispensePill()
+        self.pill_label.config(text="Pills left: "+pill.getpills())
+        dispensed = False
 
 if __name__ == "__main__":
     app = SampleApp()
-    app.update()
     app.toggle_fullscreen()
     app.mainloop()
--- python/iPill.py
+++ python/iPill.py
@@ -9,3 +9,19 @@ def dispensePill():
     ss.set_servo(1, 0)
     time.sleep(period)
     ss.set_servo(1, 180)
+
+class pillInfo():
+    pname = "NaN"
+    pinfo = "NaN"
+    dinfo = "NaN"
+    pills = 0
+    def setinfo(self,pname,pinfo,dinfo,pills):
+        self.pname = pname
+        self.pinfo = pinfo
+        self.dinfo = dinfo
+        self.pills = int(pills)
+    def getpills(self):
+        return str(self.pills)
+    def dispense(self):
+        self.pills -= 1
+             
